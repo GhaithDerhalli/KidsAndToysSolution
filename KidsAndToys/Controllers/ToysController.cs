2f66511e7e95225a8c8eff7ea5e4deb61dad3a9a
Zoheer Jalali <zoheer.jalali@hotmail.com>
2022-01-24 16:17:14 +0100

    Added controller

---
+++ KidsAndToys/Controllers/ToysController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KidsAndToys.Controllers
+{
+    public class ToysController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
