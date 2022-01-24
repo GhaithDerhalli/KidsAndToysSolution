6d0d3fcdc66ff91e77617ea22494c57b596ca2da
Anna Andrée <anna.andree91@gmail.com>
2022-01-24 16:15:21 +0100

    Add controllers, views, modells folders. Plus add in program.cs

--- KidsAndToys/Program.cs
+++ KidsAndToys/Program.cs
@@ -1,6 +1,21 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Stöd för controllers och views
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddTransient<AcmeService>(); // DI: Ny instans varje gång
+//builder.Services.AddSingleton<ProductService>(); // DI: Samma instans varje gång
+
+//var connString = builder.Configuration.GetConnectionString("AcmeConnection");
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Stöd för att mappa HTTP-anrop till våra controllers
+app.UseRouting();
+
+// Stöd för Route-attribut på våra Action-metoder
+app.UseEndpoints(o => o.MapControllers());
 
 app.Run();
