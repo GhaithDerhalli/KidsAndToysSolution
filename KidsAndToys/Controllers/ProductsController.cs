b7351634ecb8d0f1ab131a9eedd939444f4f2d47
Anna Andrée <anna.andree91@gmail.com>
2022-01-25 11:59:49 +0100

    Added links to the controller

--- KidsAndToys/Controllers/ProductsController.cs
+++ KidsAndToys/Controllers/ProductsController.cs
@@ -15,9 +15,50 @@ namespace KidsAndToys.Controllers
         [HttpGet]
         public IActionResult NewAds()
         {
-            return Content("test");
+            return Content("new ads");
             //return View();
         }
+
+        [Route("createuser")]
+        [HttpGet]
+        public IActionResult CreateUser()
+        {
+            return Content("new user");
+            //return View();
+        }
+        [Route("login")]
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return Content("login");
+            //return View();
+        }
+
+        [Route("myads")]
+        [HttpGet]
+        public IActionResult MyAds()
+        {
+            return Content("Mina annonser");
+            //return View();
+        }
+
+
+        [Route("listofads")]
+        [HttpGet]
+        public IActionResult ListOfAds()
+        {
+            return Content("lista av annonser");
+            //return View();
+        }
+
+        [Route("details")]
+        [HttpGet]
+        public IActionResult Details()
+        {
+            return Content("details");
+            //return View();
+        }
+
 
     }
 }
