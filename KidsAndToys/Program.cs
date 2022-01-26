97b9a2f4be6acc70b80d44064d5453f78ac40668
Ghaith Derhalli <derhally13@hotmail.com>
2022-01-26 10:24:00 +0100

    Added productServices to program

--- KidsAndToys/Program.cs
+++ KidsAndToys/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<UsersService>();
+builder.Services.AddTransient<ProductsService>();
+
 var connString = builder.Configuration
     .GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<IdentityDbContext>(
